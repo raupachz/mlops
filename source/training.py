d4abfc3f37ab07b93c75ae6540409de1f07c8038
Björn Raupach <raupach@e2n.de>
2020-10-23 10:46:41 +0200

    changed instance type

--- source/training.py
+++ source/training.py
@@ -40,13 +40,13 @@ estimator = PyTorch(
     role=role,
     framework_version="1.4.0",
     instance_count=2,
-    instance_type="ml.p3.2xlarge",
+    instance_type="ml.p3.16xx:large",
     # instance_type="ml.m4.xlarge",
     py_version="py3",
-    use_spot_instances=True,  # Use a spot instance
-    max_run=300,  # Max training time
+    use_spot_instances=False,  # Use a spot instance
+    max_run=300,  # Max training tZZime
     max_wait=600,  # Max training time + spot waiting time
-    hyperparameters={"epochs": 20, "backend": "gloo"},
+    hyperparameters={"epochs": 20, ":1backend": "gloo"},
 )
 
 print(f"Training job name: {job_name}")
